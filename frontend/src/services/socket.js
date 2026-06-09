80ec8b8862176a4fe9b771b35e227c35f146e953
Arbaz Ansari <arbazansari7934@gmail.com>
2026-06-09 20:08:24 +0530

    hardcoded backend url to test

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 console.log("BACKEND_URI =", import.meta.env.VITE_BACKEND_URI);
-const socket=io(import.meta.env.VITE_BACKEND_URI,{
+const socket=io("https://cryptosim-t6b4.onrender.com",{
     auth: {
     token: localStorage.getItem("token"),
     },
