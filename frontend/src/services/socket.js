0f11730bf1c56b71cf1a2a909fb5f04966e483fc
Arbaz Ansari <arbazansari7934@gmail.com>
2026-06-09 18:47:25 +0530

    fix frontend api config

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket=io("http://localhost:5000",{
+const socket=io(import.meta.env.BACKEND_URI,{
     auth: {
     token: localStorage.getItem("token"),
     },
